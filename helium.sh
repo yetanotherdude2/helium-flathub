e4346fff73d68db3d7e5712e4a1abccbad53ae33
ShyVortex <angelotr@ik.me>
2026-06-14 21:53:50 +0200

    fix: proper cobalt configuration
    Also trigger rebuild

--- helium.sh
+++ helium.sh
@@ -46,4 +46,15 @@ if [ -f "${CDM_LIB}" ]; then
     printf '{"Path":"%s"}' "${WIDEVINE_DIR}" > "${CDM_REG}"
 fi
 
-exec /app/lib/helium/helium.real --no-sandbox --test-type "$@"
+# Merge the policies with the host ones.
+for proot in "etc/chromium/policies" "etc/static/chromium/policies"; do
+  for ptype in managed recommended enrollment; do
+    if [ -d "/run/host/$proot/$ptype" ]; then
+      mkdir -p "/etc/chromium/policies/$ptype"
+      ln -sf "/run/host/$proot/$ptype"/*.json "/etc/chromium/policies/$ptype" 2>/dev/null
+    fi
+  done
+done
+
+exec cobalt --class=net.imput.helium "$@" --no-default-browser-check
+
